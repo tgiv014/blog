361db30194dfd564f6a36d4c4e178aff0a67c216
Thomas Gorham <tgiv014@gmail.com>
2023-03-06 22:43:50 -0800

    colorize inline code

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,25 @@ module.exports = {
       backgroundImage: {
         "wave-pattern": "url('/img/wave.svg')",
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "code::before": {content: ''},
+            "code::after": {content: ''},
+            "code":{
+              color: theme('colors.amber.400')
+            },
+            "h1 code": {
+              color: theme('colors.amber.400')},
+            "h2 code": {
+              color: theme('colors.amber.400')},
+            "h3 code": {
+              color: theme('colors.amber.400')},
+            "h4 code": {
+              color: theme('colors.amber.400')},
+          }
+        }
+      })
     },
   },
   plugins: [require("@tailwindcss/typography")],
