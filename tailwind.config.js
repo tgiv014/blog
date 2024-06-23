0d72d68976c35d2e6f8f53707114c4d42c61f40e
Thomas Gorham <thomas@convictional.com>
2024-06-22 20:30:25 -0600

    dang all my fonts were huge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
+const round = (num) =>
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, '$1')
+    .replace(/\.0$/, '')
+const rem = (px) => `${round(px / 16)}rem`
+const em = (px, base) => `${round(px / base)}em`
 module.exports = {
   mode: "jit",
   content: ["./content/**/*.md", "./layouts/**/*.html"],
@@ -9,8 +17,14 @@ module.exports = {
         sans: ["InterVariable", "sans-serif"]
       },
       typography: (theme) => ({
-        DEFAULT: {
+        sm: {
           css: {
+            pre: {
+              fontSize: rem(14),
+            },
+            code: {
+              fontSize: rem(14),
+            },
             // "code::before": {content: ''},
             // "code::after": {content: ''},
           }
